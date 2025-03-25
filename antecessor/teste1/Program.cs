d5e15a7e5f14a7849473425db4172a71abf9867d
Brenda Bomfim <127886150+BrendaBomfim95@users.noreply.github.com>
2025-03-25 09:26:44 -0300

    antecessor

---
+++ antecessor/teste1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {//inicio
+                int antecessor, resultado;
+                Console.Write("Digite o número desejado para verificar o antecessor ");
+                antecessor = Convert.ToInt32(Console.ReadLine());
+
+                resultado = antecessor - 1;
+                Console.Write("Este é o antecessor:" + resultado);
+                Console.ReadKey();
+
+            }
+
+            }
+    }
+}
