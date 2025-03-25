9eaaef5caef65d3c1327a229ecb18fb0fefa10c4
Brenda Bomfim <127886150+BrendaBomfim95@users.noreply.github.com>
2025-03-25 09:08:09 -0300

    atividade13

---
+++ atividade13.profgilmar/atividade13.profgilmar/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade13.profgilmar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicio
+            
+            int[] a = new int[6] { 01, 09, 17, 23, 35, 48 };
+            int[] b = new int[6];
+            int f, d, i, c = 0;
+            i = 0;
+            d = 0;
+            f = 0;
+            do
+            {
+                Console.WriteLine("entre com " + (c + 1) + "º número da aposta");
+                b[c] = Convert.ToInt32(Console.ReadLine());
+                c = c + 1;
+            } while (c < 6);
+            for (i = 0; i < 6; i++)
+            {
+                for (d = 0; d < 6; d++)
+                {
+                    if (a[i] == b[d])
+                    {
+                        f = f + 1;
+                    }
+                }
+            }
+            if (f == 6)
+            {
+                Console.WriteLine("acertou a mega sena");
+            }
+            else if (f == 6)
+            {
+                
+                Console.WriteLine("acertou a a quina");
+            }
+            else if (f == 4)
+            {
+                
+                Console.WriteLine("acertou a a quadra");
+            }
+            else if (f <= 3)
+            {
+                
+                Console.WriteLine("Não foi dessa vez");
+            }
+            Console.ReadKey();
+            
+            
+        }//fim
+
+
+
+
+
+    }
+    }
+
