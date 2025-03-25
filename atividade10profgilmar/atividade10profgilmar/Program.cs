388834d3700e24845146fc5145d98406852f24d4
Brenda Bomfim <127886150+BrendaBomfim95@users.noreply.github.com>
2025-03-25 09:07:00 -0300

    atividade10

---
+++ atividade10profgilmar/atividade10profgilmar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace atividade10profgilmar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //inicio
+            int num;
+            do
+            {
+                Console.WriteLine("Digite um numero par:");
+                num= Convert.ToInt32(Console.ReadLine());
+
+
+            } while (num % 2!=0);
+            //(num % 2!=0); quando o resto da divisão for diferente de 0, vai continuar dentro do laço.
+
+            Console.WriteLine("numero par:"+num);
+
+            Console.ReadKey();
+        }
+    }
+}
